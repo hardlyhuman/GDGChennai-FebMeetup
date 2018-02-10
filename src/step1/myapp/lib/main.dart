0f86fa880cfec702e6b98f1a3671d96149ac8b9c
SriHarshaGajavalli <sriharsha.g15@iiits.in>
2018-02-10 06:46:36 +0530

    Step1

--- src/step1/myapp/lib/main.dart
+++ src/step1/myapp/lib/main.dart
@@ -4,3 +4,14 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(new FriendlychatApp());
 }
+
+
+class FriendlychatApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: "Friendlychat",
+      home: new ChatScreen(),
+    );
+  }
+}
