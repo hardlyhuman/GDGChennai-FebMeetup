d89b56b549372d07197eddb14633044f171728d0
SriHarshaGajavalli <sriharsha.g15@iiits.in>
2018-02-10 06:58:32 +0530

    Step3

--- src/step3/myapp/lib/main.dart
+++ src/step3/myapp/lib/main.dart
@@ -1,9 +1,10 @@
-
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(new FriendlychatApp());
 }
+
+const String _name = "Your Name";
 
 class FriendlychatApp extends StatelessWidget {
   @override
@@ -15,16 +16,53 @@ class FriendlychatApp extends StatelessWidget {
   }
 }
 
+class ChatMessage extends StatelessWidget {
+  ChatMessage({this.text});
+  final String text;
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      margin: const EdgeInsets.symmetric(vertical: 10.0),
+      child: new Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          new Container(
+            margin: const EdgeInsets.only(right: 16.0),
+            child: new CircleAvatar(child: new Text(_name[0])),
+          ),
+          new Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              new Text(_name, style: Theme.of(context).textTheme.subhead),
+              new Container(
+                margin: const EdgeInsets.only(top: 5.0),
+                child: new Text(text),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
 class ChatScreen extends StatefulWidget {
   @override
   State createState() => new ChatScreenState();
 }
 
 class ChatScreenState extends State<ChatScreen> {
+  final List<ChatMessage> _messages = <ChatMessage>[];
   final TextEditingController _textController = new TextEditingController();
 
   void _handleSubmitted(String text) {
     _textController.clear();
+    ChatMessage message = new ChatMessage(
+      text: text,
+    );
+    setState(() {
+      _messages.insert(0, message);
+    });
   }
 
   Widget _buildTextComposer() {
@@ -51,13 +89,27 @@ class ChatScreenState extends State<ChatScreen> {
     );
   }
 
-  @override
   Widget build(BuildContext context) {
     return new Scaffold(
-        appBar: new AppBar(
-            title: new Text("Friendlychat")
-        ),
-        body: _buildTextComposer()
+      appBar: new AppBar(title: new Text("Friendlychat")),
+      body: new Column(
+          children: <Widget>[
+            new Flexible(
+                child: new ListView.builder(
+                  padding: new EdgeInsets.all(8.0),
+                  reverse: true,
+                  itemBuilder: (_, int index) => _messages[index],
+                  itemCount: _messages.length,
+                )
+            ),
+            new Divider(height: 1.0),
+            new Container(
+              decoration: new BoxDecoration(
+                  color: Theme.of(context).cardColor),
+              child: _buildTextComposer(),
+            ),
+          ]
+      ),
     );
   }
 }
